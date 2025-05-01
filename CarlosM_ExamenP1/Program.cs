2e7875e7198d338c51e5f9e11c0db738d6c74a45
caaarrrrlll <carlosmoreta140@gmail.com>
2025-05-01 16:01:55 -0500

    Correcion final, añadido meme, limitantes, arreglado bug que no permitia ver los otros modelos

--- CarlosM_ExamenP1/Program.cs
+++ CarlosM_ExamenP1/Program.cs
@@ -4,6 +4,7 @@ using CarlosM_ExamenP1.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
